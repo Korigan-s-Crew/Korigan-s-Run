6e742c85dece72a5028e45e1acc6084ba28494f1
JULLIEN Baptiste <baptiste.jullien@telecomnancy.eu>
2024-03-10 20:43:14 +0100

    deleted useless file from example

--- old_example/example.h
+++
@@ -1,5 +0,0 @@
-#include <SDL2/SDL.h>
-
-SDL_Texture *loadImage(const char path[], SDL_Renderer *renderer);
-int init(SDL_Window **window, SDL_Renderer **renderer, int w, int h);
-int setWindowColor(SDL_Renderer *renderer, SDL_Color color);
