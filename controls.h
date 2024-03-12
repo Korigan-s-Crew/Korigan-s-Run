efa6027b17be31e5c045e84a0767b84180fd6e7b
JULLIEN Baptiste <baptiste.jullien@telecomnancy.eu>
2024-03-12 23:45:19 +0100

    change declaration of struct Controls because my c spell_checker don't like this way in c

--- controls.h
+++ controls.h
@@ -10,13 +10,14 @@
 #include <stdlib.h>
 #include <string.h>
 
-typedef struct Controls {
+struct Controls {
     const char *layout;
     SDL_KeyCode left;
     SDL_KeyCode right;
     SDL_KeyCode up;
     SDL_KeyCode down;
-}Controls;
+};
+typedef struct Controls Controls;
 
 int switchLayout(Controls *controls);
 Controls *init_controls(void);
