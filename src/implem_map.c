807df803ae40c5c2c101e51ef2c58c0c95374f36
VignauSicard Gauthier <gauthier.vignau-sicard@telecomnancy.eu>
2024-04-25 18:11:17 +0200

    test de construction du graphe et récupération du parcours emprunté

--- src/implem_map.c
+++ src/implem_map.c
@@ -85,67 +85,69 @@ char* parcours_graphe(struct Graphe* graphe){
 
 }
 
-// FILE* create_map_txt(char* parcours){
-//     // création du fichier map.txt qui contient les patterns dans l'ordre
-//     FILE* f1 = fopen("pattern1.txt","r");
-//     FILE* f2 = fopen("pattern2.txt","r");
-//     FILE* f3 = fopen("pattern3.txt","r");
-//     FILE* f4 = fopen("pattern4.txt","r");
-//     FILE* f5 = fopen("pattern5.txt","r");
-//     FILE* f6 = fopen("pattern6.txt","r");
-//     FILE* f7 = fopen("pattern7.txt","r");
-//     FILE* f8 = fopen("pattern8.txt","r");
-//     FILE* f9 = fopen("pattern9.txt","r");
-//     FILE* f10 = fopen("pattern10.txt","r");
-//     FILE* f = fopen("map.txt", "w");
-//
-//     if (f1 == NULL || f2 == NULL || f3 == NULL || f4 == NULL || f5 == NULL || f6 == NULL || f7 == NULL || f8 == NULL || f9 == NULL || f10 == NULL || f == NULL){
-//         printf("Error opening file\n");
-//         exit(1);
-//     }
-//
-//     for (int i = 0; parcours[i] != '\0'; i++){
-//         if (parcours[i] == '1' && parcours[i+1] != '0'){
-//             concat_Files("map.txt", "pattern1.txt");
-//         }
-//         if (parcours[i] == '2'){
-//             concat_Files("map.txt", "pattern2.txt");
-//         }
-//         if (parcours[i] == '3'){
-//             concat_Files("map.txt", "pattern3.txt");
-//         }
-//         if (parcours[i] == '4'){
-//             concat_Files("map.txt", "pattern4.txt");
-//         }
-//         if (parcours[i] == '5'){
-//             concat_Files("map.txt", "pattern5.txt");
-//         }
-//         if (parcours[i] == '6'){
-//             concat_Files("map.txt", "pattern6.txt");
-//         }
-//         if (parcours[i] == '7'){
-//             concat_Files("map.txt", "pattern7.txt");
-//         }
-//         if (parcours[i] == '8'){
-//             concat_Files("map.txt", "pattern8.txt");
-//         }
-//         if (parcours[i] == '9'){
-//             concat_Files("map.txt", "pattern9.txt");
-//         }
-//         if (parcours[i] == '1' && parcours[i+1] == '0'){
-//             concat_Files("map.txt", "pattern10.txt");
-//         }
-//     }
-//     return f;
-// }
+FILE* create_map_txt(char* parcours){
+    // création du fichier map.txt qui contient les patterns dans l'ordre
+    FILE* f1 = fopen("../Patterns/pattern1.txt","r");
+    FILE* f2 = fopen("../Patterns/pattern2.txt","r");
+    FILE* f3 = fopen("../Patterns/pattern3.txt","r");
+    FILE* f4 = fopen("../Patterns/pattern4.txt","r");
+    FILE* f5 = fopen("../Patterns/pattern5.txt","r");
+    FILE* f6 = fopen("../Patterns/pattern6.txt","r");
+    FILE* f7 = fopen("../Patterns/pattern7.txt","r");
+    FILE* f8 = fopen("../Patterns/pattern8.txt","r");
+    FILE* f9 = fopen("../Patterns/pattern9.txt","r");
+    FILE* f10 = fopen("../Patterns/pattern10.txt","r");
+    FILE* f = fopen("../test.txt", "w");
+
+    if (f1 == NULL || f2 == NULL || f3 == NULL || f4 == NULL || f5 == NULL || f6 == NULL || f7 == NULL || f8 == NULL || f9 == NULL || f10 == NULL || f == NULL){
+        printf("Error opening file\n");
+        exit(1);
+    }
+
+    for (int i = 0; parcours[i] != '\0'; i++){
+        if (parcours[i] == '1' && parcours[i+1] != '0'){
+            concat_Files("map.txt", "pattern1.txt");
+        }
+        if (parcours[i] == '2'){
+            concat_Files("map.txt", "pattern2.txt");
+        }
+        if (parcours[i] == '3'){
+            concat_Files("map.txt", "pattern3.txt");
+        }
+        if (parcours[i] == '4'){
+            concat_Files("map.txt", "pattern4.txt");
+        }
+        if (parcours[i] == '5'){
+            concat_Files("map.txt", "pattern5.txt");
+        }
+        if (parcours[i] == '6'){
+            concat_Files("map.txt", "pattern6.txt");
+        }
+        if (parcours[i] == '7'){
+            concat_Files("map.txt", "pattern7.txt");
+        }
+        if (parcours[i] == '8'){
+            concat_Files("map.txt", "pattern8.txt");
+        }
+        if (parcours[i] == '9'){
+            concat_Files("map.txt", "pattern9.txt");
+        }
+        if (parcours[i] == '1' && parcours[i+1] == '0'){
+            concat_Files("map.txt", "pattern10.txt");
+        }
+    }
+    return f;
+}
 
 int main(){
-    //struct Graphe* graphe = create_graph();
-    //char* parcours = parcours_graphe(graphe);
-    //FILE* f = create_map_txt(parcours);
-    //free_graph(graphe);
-    //free(parcours);
-    //fclose(f);
-    concat_Files("../Patterns/pattern1.txt", "../Patterns/pattern2.txt");
+    struct Graphe* graphe = create_graph();
+    printf("Graphe créé\n");
+    char* parcours = parcours_graphe(graphe);
+    printf("%s\n", parcours);
+    FILE* f = create_map_txt(parcours);
+    free_graph(graphe);
+    free(parcours);
+    fclose(f);
+    //concat_Files("../Patterns/pattern1.txt", "../Patterns/pattern2.txt");
     return 0;
 }
