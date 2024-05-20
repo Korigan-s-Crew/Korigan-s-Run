40a0658eaaadc2e9246254db84db8ba4b0e604cf
VignauSicard Gauthier <gauthier.vignau-sicard@telecomnancy.eu>
2024-05-20 23:55:21 +0200

    un peu de musique pendant qu'on joue

---
+++ include/music.h
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <SDL2/SDL_mixer.h>
+
+#define MUSIC 1
+
+// Fonction pour lire un fichier audio
+void read_audio_file(char* filepath);
+
+void play_music();
+
+int main();
+
+// il faut installer la bibliothèque SDL2_mixer pour compiler ce fichier : 
+// libsdl2-mixer-dev 
+
+// la ligne de commande pour compiler le fichier .c associé est :
+// clang music.c -o music -L/opt/homebrew/lib -lSDL2 -lSDL2_mixer 
+
+// la ligne de commande pour exécuter le fichier .c associé est :
+// ./music
---
+++ src/music.c
@@ -0,0 +1,59 @@
+#include "../include/music.h"
+// #include </System/Volumes/Data/opt/homebrew/include/SDL2/SDL_mixer.h>
+
+// Fonction pour lire un fichier audio
+void read_audio_file(char* filepath) {
+
+    // Initialisation de SDL
+    if (SDL_Init(SDL_INIT_AUDIO) < 0) {
+        printf("Failed to initialize SDL: %s\n", SDL_GetError());
+        exit(-1);
+    }
+
+    // Ouverture du périphérique audio avec : 
+    // une fréquence d'échantillonnage de 44100 Hz, 
+    // un format audio par défaut de 16 bits, 
+    // 2 canaux pour le stéréo et 
+    // un buffer (tampon audio) de 2048 octets
+
+    if (Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048) < 0) {
+        printf("Failed to open audio: %s\n", Mix_GetError());
+        exit(-1);
+    }
+
+    // Chargement du fichier audio
+    Mix_Music *music = Mix_LoadMUS(filepath);
+
+
+    if (music == NULL) {
+        printf("Failed to load music: %s\n", Mix_GetError());
+        exit(-1);
+    }
+
+    // Lecture du fichier audio
+    // Deuxième argument : nombre de répétitions ; -1 pour une lecture infinie
+    Mix_PlayMusic(music, 1);
+
+    // Attente de la fin de la lecture
+    while (Mix_PlayingMusic()) {
+        SDL_Delay(100);
+    }
+
+    // Libération de la mémoire
+    Mix_FreeMusic(music);
+    Mix_CloseAudio();
+    SDL_Quit();
+}
+
+void play_music(){
+    music_path = "./Music/Transforyou.mp3";
+    if MUSIC == 1 {
+        read_audio_file(music_path);
+    }
+}
+
+int main() {
+    // Mix_VolumeMusic(MIX_MAX_VOLUME / 2);
+    play_music();
+    return 0;
+}
